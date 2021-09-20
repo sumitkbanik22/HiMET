c15f383b99b7ba0049dffd701d7310a2fda9ed3f
Sumit Banik <sumit@apenning.xyz>
2021-09-20 11:34:30 +0530

    added changes for right-sidebar

--- himet-frontend-react/src/components/rightsidebar/Rightsidebar.js
+++ himet-frontend-react/src/components/rightsidebar/Rightsidebar.js
@@ -3,7 +3,57 @@ import "./rightsidebar.css";
 export default function Rightsidebar() {
     return (
         <div className="right-sidebar">
-           rightbar 
+           <div className="right-sidebar-wrapper">
+               <div className="birthday-container">
+                   <img className="birthday-img" src="/assets/gift.png" alt="" />
+                   <span className="birthday-text">
+                       {" "}
+                       <b>Nayanjyoti Das </b> and <b>3 others friends </b> have birthday today.
+                   </span>
+               </div>
+               <img className="right-sidebar-ad" src="/assets/ad.png" alt="advertisement" />
+               <h4 className="right-sidebar-title">
+                   Online Friends
+               </h4>
+               <ul className="right-sidebar-friend-list">
+                   <li className="right-sidebar-friend">
+                       <div className="right-sidebar-profile-img-container">
+                           <img className="right-sidebar-profile-img" src="/assets/person/3.jpeg" alt="" />
+                           <span className="right-sidebar-online">
+                               
+                           </span>
+                       </div>
+                       <span className="right-sidebar-username">Nayanjyoti Das</span>
+                   </li>
+                   <li className="right-sidebar-friend">
+                       <div className="right-sidebar-profile-img-container">
+                           <img className="right-sidebar-profile-img" src="/assets/person/3.jpeg" alt="" />
+                           <span className="right-sidebar-online">
+                               
+                           </span>
+                       </div>
+                       <span className="right-sidebar-username">Nayanjyoti Das</span>
+                   </li>
+                   <li className="right-sidebar-friend">
+                       <div className="right-sidebar-profile-img-container">
+                           <img className="right-sidebar-profile-img" src="/assets/person/3.jpeg" alt="" />
+                           <span className="right-sidebar-online">
+                               
+                           </span>
+                       </div>
+                       <span className="right-sidebar-username">Nayanjyoti Das</span>
+                   </li>
+                   <li className="right-sidebar-friend">
+                       <div className="right-sidebar-profile-img-container">
+                           <img className="right-sidebar-profile-img" src="/assets/person/3.jpeg" alt="" />
+                           <span className="right-sidebar-online">
+                               
+                           </span>
+                       </div>
+                       <span className="right-sidebar-username">Nayanjyoti Das</span>
+                   </li>
+               </ul>
+           </div>
         </div>
     )
 }
